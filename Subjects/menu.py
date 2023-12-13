686aeec1edf944ef81c3d7c37de189a74a653229
Lucas Emanuel Nogueira de Queiroz <20221094010004@ifrn.local>
2023-12-13 10:23:08 -0300

    Tudo fudido

--- Subjects/menu.py
+++ Subjects/menu.py
@@ -1,71 +1,16 @@
 from tkinter import *
-import pickle
+from Subjects.abstract_window import Window
+from User_menu.register_user import Register
 
-def printar():
-    print("DEU CERTO")
 
-def printar_agradecidamente():
-    print("OBRIGAADOOOOOOOOOOOOOOOOOOOOOO")
-class Menu:
-    def __init__(self, mestre, title, button_quant, button_names, button_func):
-        self.title = title
-        self.button_quant = button_quant
-        self.button_names = button_names
-        self.file_name = "Pickle_files\list_register.txt"
-        self.file_name_treaths = "Pickle_files\list_treaths.txt"
-        self.list_users = []
-        self.list_treaths = []
+class Menu(Window):
+    def __init__(self, mestre, title, label_quants, label_names, button_quant, button_names):
+        super().__init__(mestre, title, label_quants, label_names, button_quant, button_names)
 
-        self.mestre = mestre
-        self.c1 = Frame(self.mestre)
-        self.c1["padx"] = 100
-        self.c1["pady"] = 10
-        self.c1.pack()
+        self.buttons_generate(self.open_file(Register))
 
-        self.titulo = Label(self.c1, text = self.title)
-        self.titulo["font"] = ("Arial", "10", "bold")
-        self.titulo.pack()
-
-        for buttons in range(self.button_quant):
-
-            self.c0 = Frame(self.mestre)
-            self.c0.pack()
-            self.botao = Button(self.c0, text = button_names[buttons])
-            self.botao["command"] = button_func[buttons]
-            self.botao.pack()
-            
-    def open_file(self,Object):
-        raiz = Tk()
-        Object(raiz)
-        raiz.mainloop()
-
-    def exibition(self,list_elements, title, attribute):
-        with open(self.file_name, 'rb') as file: 
-            while True:
-                try:
-                    list_elements = pickle.load(file)
-
-                except EOFError:
-                    break
-
-        exibition = ''
-        for element in list_elements:
-            if element and hasattr(element, attribute):
-                exibition += element.attribute + '\n'
-
-    
-        self.top = Toplevel()
-        self.top.title(title)
-
-        self.widget1 = Frame(self.top)
-        self.widget1["padx"] = 100
-        self.widget1["pady"] = 10
-        self.widget1.pack()
-        self.msg = Label(self.widget1, text= exibition)
-        self.msg.pack()
-    
 
 if __name__ == "__main__":
     raiz = Tk()
-    Menu(raiz,'teste',2,['Botao 1', 'Botao 2'], [printar, printar_agradecidamente])
+    Menu(raiz, "Hunter Website", 0, 0, 1, "Registrar")
     raiz.mainloop()
--- hunter_website.py
+++ hunter_website.py
@@ -1,43 +1,9 @@
 from tkinter import *
+from Subjects.abstract_window import Window
 from User_menu.register_user import Register
-from User_menu.login import Login
-
-class Website:
-    def __init__(self,mestre):
-        self.mestre = mestre
-        self.c1 = Frame(self.mestre)
-        self.c1["padx"] = 100
-        self.c1["pady"] = 10
-        self.c1.pack()
-
-        self.titulo = Label(self.c1, text = "Bem vindo ao Hunter Website\n ")
-        self.titulo["font"] = ("Arial", "10", "bold")
-        self.titulo.pack()
-
-        self.c2 = Frame(self.mestre)
-        self.c2.pack()
-        self.botao_registro = Button(self.c2, text = "Registro")
-        self.botao_registro["command"] = self.abrir_registro
-        self.botao_registro.pack()
-
-        self.c3 = Frame(self.mestre)
-        self.c3.pack()
-        self.botao_login = Button(self.c3, text = "Login")
-        self.botao_login["command"] = self.abrir_login
-        self.botao_login.pack()
-
-
-    def abrir_registro(self):
-        raiz_registro = Tk()
-        Register(raiz_registro)
-        raiz_registro.mainloop()
-
-    def abrir_login(self):
-        raiz_login = Tk()
-        Login(raiz_login)
-        raiz_login.mainloop()
+from Subjects.menu import Menu
 
 if __name__ == "__main__":
     raiz = Tk()
-    Website(raiz)
+    Hunter_website = Menu(raiz, "Hunter Website", 0, 0, 1, "Registrar")
     raiz.mainloop()
